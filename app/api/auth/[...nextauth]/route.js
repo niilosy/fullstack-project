77dd01cfabafbbc3d317a22bca6bc335d622a3ac
Niilo Syvänen <niilo.j.syvanen@gmail.com>
2025-05-17 18:50:48 +0300

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,26 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
-
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (token?.email) {
+        session.user.email = token.email;
+      }
+      return session;
+    },
+    async jwt({ token, profile }) {
+      if (profile?.email) {
+        token.email = profile.email;
+      }
+      return token;
+    },
+  },
+});
